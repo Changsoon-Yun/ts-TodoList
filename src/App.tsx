6308039516eb5ce6066cb93b49f3a4200d144951
Changsoon-YUN <changsoonyun@naver.com>
2022-09-19 19:30:06 +0900

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
-import useBookSearch from "./useBookSearch";
 
 function App() {
-  const [query, setQuery] = useState("");
-  const [pageNumber, setPageNumber] = useState(1);
-  const { books, hasMore, loading, error } = useBookSearch(query, pageNumber);
-  console.log(books);
-
-  function handleSearch(e: any) {
-    setQuery(e.target.value);
-    setPageNumber(1);
-  }
   return (
     <>
-      <input type="text" onChange={handleSearch} />
-
-      {books.map((book: string) => {
-        return <div key={book}>{book}</div>;
-      })}
-      <div>{loading && "Loading..."}</div>
-      <div>{error && "Error"}</div>
+      <div>typescript 기초</div>
     </>
   );
 }
