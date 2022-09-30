099f4799f9932bcbfc93af6252a1e841ad9ca37e
Changsoon-YUN <changsoonyun@naver.com>
2022-10-01 06:25:12 +0900

    tsc 기본문법

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import React, { useEffect, useState } from "react";
 
 function App() {
+  type Member = {
+    [key: string]: string;
+  };
+  let john: Member = { name: "kim", age: "23" };
+
+  console.log(john);
+
+  function a(x: number): number {
+    return x * 2;
+  }
+  console.log(a(2));
   return (
     <>
       <div>typescript 기초</div>
