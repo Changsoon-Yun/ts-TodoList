fc345c8cfd7307fb426a79b852964ff0d6570a42
changsoonyun <changsoonyun@naver.com>
2022-10-09 17:41:17 +0900

    #1 fix: 옵셔널 체이닝 및 타입추가로 버그수정

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,21 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import instance from "../axios/axios";
+import { todosApi } from "./../axios/todosApi";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
 
 const Main = () => {
-  return <div className="bg-yellow-500">안녕하세요</div>;
+  // const getTodo = async () => {
+  //   const response = await todosApi.getTodoList();
+  //   console.log(response);
+  // };
+
+  // useEffect(() => {
+  //   getTodo();
+  // }, []);
+
+  const queryClient = useQueryClient();
+
+  const query = useQuery("todos", todosApi.getTodoList);
+
+  console.log(query.data?.data[0]);
+
+  return (
+    <div>
+      <div>
+        <h2>오늘 할일</h2>
+        <p>10월 9일 일요일</p>
+        {query.data?.data.map((data: { title: string; id: number }) => (
+          <div key={data.id}>{data.title}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Main;
